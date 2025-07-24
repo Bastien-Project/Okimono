0b610816c902ccd15f33eece67b9c49dff498291
Bastien-Project <b.thomas30712@gmail.com>
2025-07-24 17:44:43 +0200

    FIX: refonte page principale DEL: anciennes images

--- index.php
+++ index.php
@@ -1,79 +1,48 @@
-<?php 
+<?php
 $base = '';
 include('./pages/header.php'); ?>
 
 <section>
-	<h2>Notre histoire</h2>
+	<h2>Pourquoi ce site ?</h2>
 	<div>
 		<div>
-				<img src="images/cadre.png" alt="cadre">
+			<img src="images/passions.png" alt="Mes passions">
 		</div>
 		<div>
 			<p>
-				Tout commença un jour. Quel jour ? Je ne m'en souviens plus ! <br>
-				Mais l'histoire raconte qu'un prof donna un projet à ses élèves et qu'ils devaient former des
-				groupes et c'est là qu'un groupe de 4 étudiants se forma. <br>
-				Ces 4 héros sont Oudja aka Dr Strange, Mathieu alias Captain Marvel, Raphaël as Hawkeye, Bastien dit
-				Ant-man. <br>
-				Captain Marvel n'intervenant que quand les 3 autre Avengers n'y arrive pas en raison de son
-				potentiel énorme, elle aurait térasser Thanos dès le début et le film serait terminé en 10 minutes.
-				<br>
-				Ces valeureux guerriers ont du accomplir chacun une tâche afin de vaincre leur Thanos à eux, le
-				fameux projet. <br>
-				Leur oeuvre étant considéré comme la 8ème merveille du monde, c'est donc évident qu'une foule s'est
-				interessé à son histoire, mais très peu connaissent la vérité. <br>
+				Okimono 置物 est un site où je partage mes passions, mes créations et mes réflexions sur divers sujets. <br>
+				Il y a une partie concernant le sport automobile, j'adore depuis depuis les courses automobiles, la F1 et aussi le karting que je pratique en loisir. <br>
+				Une autre section concerne le ski, que je pratique depuis tout petit, et chaque année j'attend avec impatience l'hiver uniquement pour pouvoir aller skier. <br>
+				Un troisième sujet sur le code et la programation, avec en lien tout mes projets de développement web. <br>
+				Et enfin un dernier thème sur le bois. Mes création, du croquis à la réalisation finale, meubles ou simple objet de décoration.
 			</p>
 		</div>
 	</div>
 
-	<h3>Pourquoi tout le monde vénère t-il Chuck Norris, alors que Bob l'éponge fait mieux, il fait cuire des steaks
-		sous l'eau ?</h3>
+	<h3>Qui suis-je ?</h3>
 	<div>
 		<div>
 			<p>
-				Chuck Norris s'est bien fait connaître grâce à la série Walker Texas Ranger qui commença en 1993.
-				<br>
-				C'est surtout grâce à ça qui s'est suivi tout les mêmes sur Chuck Norris et de là viens cette
-				"vénération". <br>
-				Et puis son public est clairement adulte pour les scènes de violence que la série peux comportée.
-				<br>
-				Alors que Bob l'Eponge, n'a commencé que en 1999. Il est donc arrivé bien après notre célèbre Chuck
-				Norris. <br>
-				Et même si les scénaristes ont voulu le rendre impressionnant en pouvant faire des barbecue sous
-				l'eau. <br>
-				Mais il peux également aller à la mer alors qu'il est déjà dans la mer, ou passer son permis
-				voiture, une voiture sous l'eau (et qui fonctionne). <br>
-				Malgré les efforts, Bob l'Eponge reste avec un public jeune qui ne relève pas forcément ça. <br>
-				Mais sutout Bob l'Eponge reste un personnage fictif, alors que Chuck Norris est bien vivant. <br>
+				Je me nomme Bastien, j'ai 25 ans et je suis passionné par le sport automobile, le ski, la programmation et le travail du bois. <br>
+				Et j'ai apparement beaucoup de temps libre, donc j'ai décidé de créer ce site pour passer le temps à ne rien faire.
 			</p>
 		</div>
 		<div>
-				<img src="images/norrisbob.jpg" alt="norris vs bob">
+			<img src="images/moi.png" alt="Ma tête">
 		</div>
 	</div>
 
-	<h3>Pourquoi la pierre qui roule n'ammasse pas mousse ?</h3>
+	<h3>Si nous, humains, voyons l'eau mais pas l'air, es-ce qu'un poisson vois l'air et pas l'eau ?</h3>
 	<div>
 		<div>
-			
-				<img src="images/pierreMousse.png" alt="pierre mousse">
-			
+
+			<img src="images/air_eau.png" alt="Mi-air mi-eau">
+
 		</div>
 		<div>
 			<p>
-				Ici aucun rapport avec la petite mousse en terasse, ou à la mer la "mousse" (l'écume) ou le jeune
-				marin encore mineur ou encore la mousse pour le confort dans nos sièges, canapés, ... <br>
-				Non dans notre question il s'agit de la mousse verdâtre que l'on trouve en forêt, qui apparaît
-				toujours du côté Nord, côté le plus humide. Un bon moyen pour se repérer quand on est perdu en
-				forêt. <br>
-				Une pierre qui roule n'ammasse pas mousse, car elle ne reste pas assez longtemps dans un coin humide
-				pour développer cette humidité. <br>
-				<strong>MAIS</strong> <br>
-				Il faut vraiment que la pierre roule pendant longtemps pour ne jamais amasser de mousse, et c'est
-				compliqué de faire tomber une pierre pendant des décennies. <br>
-				Au pire la mettre dans des conditions extrêmes (désert aride ou haute altitude en montagne) fera
-				qu'elle n'aura jamais de mousse par manque d'humidité mais dans ce cas elle ne roule quand même pas.
-				<br>
+				Et ben je n'en ai aucune idée.
+				<br> Mais c'est une des questions que je me pose et dont je n'aurais pas de réponse.
 			</p>
 		</div>
 	</div>
@@ -84,36 +53,39 @@ include('./pages/header.php'); ?>
 <div class="block2">
 	<div class="wrap">
 		<div class="tile">
-			<img src='./images/end.jpg' />
+			<div class="img-coin haut-gauche"></div>
 			<div class="text">
-				<h1>Ecologie</h1>
-				<h2 class="animate-text">Utilisation de matériaux recyclé</h2>
-				<p class="animate-text">Les different plastique utilisé pour nos impression sont recyclé et/ou
-					biodegradable fabriqué a l'aide d'amidon de mais. Les surplus de plastique sont traité par
-					une entrepise annexe.</p>
+				<h1>Sport automobile</h1>
+				<h2 class="animate-text">La F1 et le karting</h2>
+				<p class="animate-text">Je suis un fan de Formule 1 depuis petit. Et arrivé à l'adolescence, j'ai commencé à faire du karting.</p>
 			</div>
 		</div>
 
 
 		<div class="tile">
-			<img src='./images/end.jpg' />
+			<div class="img-coin haut-droit"></div>
 			<div class="text">
-				<h1>Economie</h1>
-				<h2 class="animate-text">Soyez gagnant et nous aussi</h2>
-				<p class="animate-text">Nous disposont d'une grand quantité de stock et pouvons vous offrir un
-					prix/kg imbattable. Nos machine possède une classe energetique A et donc une emprunte CO2
-					tres faible.</p>
+				<h1>Ski</h1>
+				<h2 class="animate-text">Passion pour la neige</h2>
+				<p class="animate-text">Je pratique le ski quasiment tous les hivers depuis ma naissance. Il n'y a pas une année où je ne suis pas allé skier.</p>
 			</div>
 		</div>
 
 		<div class="tile">
-			<img src='./images/end.jpg' />
+			<div class="img-coin bas-gauche"></div>
 			<div class="text">
-				<h1>Rapidité</h1>
-				<h2 class="animate-text">Prise en charge et envoie rapide</h2>
-				<p class="animate-text">Nous nous efforcons de prendre en charge vos commande le plus rapidement
-					possible afin de limité la perte de temps due a l'impression. Notre service de livraison es
-					finement choisi.</p>
+				<h1>Programation</h1>
+				<h2 class="animate-text">Développement web et logiciel</h2>
+				<p class="animate-text">Je suis passionné par le développement. Je passe mon temps libre à coder et à créer des projets décalés ou plus sérieux.</p>
+			</div>
+		</div>
+
+		<div class="tile">
+			<div class="img-coin bas-droit"></div>
+			<div class="text">
+				<h1>Menuiserie</h1>
+				<h2 class="animate-text">Mes créations</h2>
+				<p class="animate-text">Je réalise des meubles sur mesure et des objets de décoration en bois. Je travaille la majorité du temps avec du bois récupéré quelque part, non acheté en magasin.</p>
 			</div>
 		</div>
 	</div>
