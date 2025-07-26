03143381ee9411d06ad3ba89a087dc96d97f3d24
Bastien-Project <b.thomas30712@gmail.com>
2025-07-26 15:26:26 +0200

    FIX: ajout de styles et mise en ordre des projets

--- pages/menuiserie.php
+++ pages/menuiserie.php
@@ -2,140 +2,245 @@
 $base = '../';
 include("./header.php"); ?>
 
-<!-- GROSSO MODO, a remettre dans l'ordre chronologique -->
+<section class="realisations">
+    <p>Voici mes créations en bois au fil des années :</p>
 
-<p>Voici quelques-unes de mes créations en bois :</p>
-<!-- premier meuble et sa v2 -->
-<div class="creation">
-    <h2>Premier meuble en bois</h2>
-    <p>J'ai réalisé un meuble en bois sur mesure, qui allie fonctionnalité et esthétique.</p>
-    <img src="../images/meuble1.png" alt="Meuble en bois" />
-    <p>Ce meuble est conçu pour s'adapter parfaitement à mon espace de vie.</p>
-    <!-- et sa v2 -->
-    <h2>Et la v2</h2>
-    <p>Voici la version améliorée de mon premier meuble, avec des finitions et des détails supplémentaires.</p>
-    <img src="../images/meuble1_v2.png" alt="Meuble en bois v2" />
-    <p>Cette version met en avant mon évolution dans la menuiserie et mon souci du détail.</p>
-</div>
-<!-- meuble déco love -->
-<div class="creation">
-    <h2>Meuble Déco Love</h2>
-    <p>Ce meuble est une pièce unique, réalisée avec amour et passion.</p>
-    <img src="../images/meuble_deco_love.png" alt="Meuble Déco Love" />
-    <p>Il apporte une touche chaleureuse et romantique à mon intérieur.</p>
-</div>
+    <!-- renovation table extérieur en tek-->
+    <div class="creation">
+        <h2>Rénovation Table Extérieur en Tek</h2>
+        <p>J'ai rénové une table d'extérieur en bois de tek, lui redonnant vie et éclat.</p>
+        <!--img src="../images/table_exterieur_tek.png" alt="Rénovation Table Extérieur en Tek" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>Désolé de la mauvaise qualité de la photo. Depuis cette table a été découpée et jetée ou sert pour la cheminée l'hiver.</p>
+    </div>
 
-<!-- ratelier de ski -->
-<div class="creation">
-    <h2>Ratelier de Ski</h2>
-    <p>Ce ratelier est conçu pour ranger et exposer mes skis avec style.</p>
-    <img src="../images/ratelier_ski.png" alt="Ratelier de Ski" />
-    <p>Il allie praticité et esthétisme, tout en mettant en valeur mes équipements de ski.</p>
-</div>
-<!-- meuble de pied enceinte -->
-<div class="creation">
-    <h2>Meuble de Pied Enceinte</h2>
-    <p>Ce meuble est conçu pour accueillir mes enceintes tout en s'intégrant harmonieusement dans mon intérieur.</p>
-    <img src="../images/meuble_pied_enceinte.png" alt="Meuble de Pied Enceinte" />
-    <p>Il combine design moderne et fonctionnalité.</p>
-</div>
+    <!-- premier meuble -->
+    <div class="creation">
+        <h2>Premier meuble en bois</h2>
+        <p>J'ai réalisé un meuble en bois sur mesure, son but était de pouvoir ranger mes consoles et leurs jeux.</p>
+        <!--img src="../images/meuble1.png" alt="Meuble en bois" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>Ceci est une reconstitution, n'ayant pas pris de photo à l'époque. Depuis il a été démonté et jeté.</p>
+    </div>
 
-<!-- meuble rouleau PQ -->
-<div class="creation">
-    <h2>Meuble Rouleau PQ</h2>
-    <p>Ce meuble est une solution pratique et esthétique pour ranger les rouleaux de papier toilette.</p>
-    <img src="../images/meuble_rouleau_pq.png" alt="Meuble Rouleau PQ" />
-    <p>Il ajoute une touche d'originalité à ma salle de bain.</p>
-</div>
+    <!-- cadre en tasseau pour tableau -->
+    <div class="creation">
+        <h2>Cadre en Tasseau pour Tableau</h2>
+        <p>J'ai fabriqué des cadres en tasseau pour des tableau que j'ai commandé mais il n'y avait pas le cadre avec.</p>
+        <!--img src="../images/cadre_tasseau.png" alt="Cadre en Tasseau pour Tableau" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>Avec un petit fil dérrière on peux les accrocher n'importe où avec un petit clou.</p>
+    </div>
 
-<!-- renovation table extérieur en tek-->
-<div class="creation">
-    <h2>Rénovation Table Extérieur en Tek</h2>
-    <p>J'ai rénové une table d'extérieur en bois de tek, lui redonnant vie et éclat.</p>
-    <img src="../images/table_exterieur_tek.png" alt="Rénovation Table Extérieur en Tek" />
-    <p>Cette table est maintenant prête à accueillir des repas en plein air.</p>
-</div>
+    <!-- meuble console -->
+    <div class="creation">
+        <h2>Meuble Console</h2>
+        <p>Ce meuble est conçu pour accueillir mes consoles de jeux et leurs accessoires. Une sorte de V2 de mon premier meuble.</p>
+        <!--img src="../images/meuble_console.png" alt="Meuble Console" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>Depuis il existe encore et sert toujours à la même fonction.</p>
+    </div>
 
-<!-- renovation petite table tek + banc ectérieur -->
-<div class="creation">
-    <h2>Rénovation Petite Table Tek + Banc Extérieur</h2>
-    <p>J'ai rénové une petite table en tek et un banc d'extérieur, les transformant en pièces uniques.</p>
-    <img src="../images/renovation_table_banc_tek.png" alt="Rénovation Petite Table Tek + Banc Extérieur" />
-    <p>Ces meubles sont parfaits pour profiter du jardin.</p>
-</div>
+    <!-- ratelier de ski -->
+    <div class="creation">
+        <h2>Ratelier de Ski</h2>
+        <p>Ce ratelier est conçu pour ranger les skis, les bâtons mais aussi les chaussures de ski. Capacité étudiée pour 5 paires de ski ou 3 paires de ski et 2 snowboards.</p>
+        <!--img src="../images/ratelier_ski.png" alt="Ratelier de Ski" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>Depuis il y a eu un dégats des eaux et il reste à refaire.</p>
+        <!--img src="../images/ratelier_ski_moisi.png" alt="Ratelier de Ski 2" /-->
+        <div class="img-coin bas-droit"></div>
+    </div>
 
-<!-- meuble porte épide -->
-<div class="creation">
-    <h2>Meuble Porte Épices</h2>
-    <p>Ce meuble est conçu pour organiser et exposer mes épices de manière pratique et esthétique.</p>
-    <img src="../images/meuble_porte_epices.png" alt="Meuble Porte Épices" />
-    <p>Il ajoute une touche de couleur et de fonctionnalité à ma cuisine.</p>
-</div>
+    <!-- découpe sur mesure anti rat -->
+    <div class="creation">
+        <h2>Découpe sur Mesure Anti Rat</h2>
+        <p>J'ai réalisé une découpe sur mesure au niveau des tuiles pour éviter qu'un rat passe.</p>
+        <!--img src="../images/decoupe_anti_rat.png" alt="Découpe sur Mesure Anti Rat" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>Cette solution est à la fois pratique et esthétique.</p>
+    </div>
 
-<!-- meuble porte clé -->
-<div class="creation">
-    <h2>Meuble Porte Clés</h2>
-    <p>Ce meuble est une solution pratique pour organiser mes clés tout en ajoutant une touche décorative à mon entrée.</p>
-    <img src="../images/meuble_porte_cles.png" alt="Meuble Porte Clés" />
-    <p>Il est à la fois fonctionnel et esthétique.</p>
-</div>
+    <!-- meuble de pied enceinte -->
+    <div class="creation">
+        <h2>Pied d'enceinte</h2>
+        <p>Ces deux petits supports sont conçus pour accueillir mes enceintes tout en s'intégrant harmonieusement dans mon intérieur.</p>
+        <!--img src="../images/meuble_pied_enceinte.png" alt="Meuble de Pied Enceinte" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>Toujours utilisés, je m'en sers de rangement un peu en dessous en plus.</p>
+    </div>
 
-<!-- meuble table banc, 2 en 1 -->
-<div class="creation">
-    <h2>Meuble Table Banc</h2>
-    <p>Ce meuble est une pièce polyvalente qui peut servir de table ou de banc selon les besoins.</p>
-    <img src="../images/meuble_table_banc.png" alt="Meuble Table Banc" />
-    <p>Il est idéal pour les petits espaces et ajoute une touche de style à mon intérieur.</p>
-</div>
+    <!-- table basse palette -->
+    <div class="creation">
+        <h2>Table Basse Palette</h2>
+        <p>J'ai fabriqué une table basse à partir d'une palette recyclée.</p>
+        <!--img src="../images/table_basse_palette.png" alt="Table Basse Palette" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>Ceci est une reconstitution, je n'ai qu'une photo quand je la démontée pour la jeter.</p>
+        <p>Car je n'avais pas pensé que pour manger les trous pouvaient être gênants quand je l'ai pensée.</p>
+    </div>
 
-<!-- meuble vide poche -->
-<div class="creation">
-    <h2>Meuble Vide Poche</h2>
-    <p>Ce meuble est conçu pour accueillir mes petits objets du quotidien, tout en ajoutant une touche décorative à mon intérieur.</p>
-    <img src="../images/meuble_vide_poche.png" alt="Meuble Vide Poche" />
-    <p>Il est à la fois pratique et esthétique.</p>
-</div>
+    <!-- meuble déco love -->
+    <div class="creation">
+        <h2>Meuble Déco Love</h2>
+        <p>Ce meuble est une pièce unique, réalisée avec amour et passion.</p>
+        <!--img src="../images/meuble_deco_love.png" alt="Meuble Déco Love" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>C'était un défi de réussir à la fabriqué, depuis il un meuble de décoration qui prend la poussière.</p>
+    </div>
 
-<p> Et voici mes futurs projets :</p>
-<!-- meuble porte manteau -->
-<div class="creation">
-    <h2>Meuble Porte Manteau</h2>
-    <p>Ce meuble est conçu pour accueillir mes manteaux et accessoires avec style.</p>
-    <img src="../images/meuble_porte_manteau.png" alt="Meuble Porte Manteau" />
-    <p>Il optimise l'espace de mon entrée tout en ajoutant une touche décorative.</p>
-</div>
+    <!-- meuble rouleau PQ -->
+    <div class="creation">
+        <h2>Meuble Rouleau PQ</h2>
+        <p>Ce meuble est une solution pratique à une réel problème, je n'avais pas de dérouleur de papier toilette et impossible d'en mettre un.</p>
+        <!--img src="../images/meuble_rouleau_pq.png" alt="Meuble Rouleau PQ" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>Petit tips, pour bloquer le distributeur sans le condamner, un simple tourillon.</p>
+    </div>
 
-<!-- meuble range mug -->
-<div class="creation">
-    <h2>Meuble Range Mug</h2>
-    <p>Ce meuble est conçu pour organiser et exposer mes mugs de manière pratique et esthétique.</p>
-    <img src="../images/meuble_range_mug.png" alt="Meuble Range Mug" />
-    <p>Il ajoute une touche de couleur et de fonctionnalité à ma cuisine.</p>
-</div>
+    <!-- rajour bureau -->
+    <div class="creation">
+        <h2>Rajout Bureau</h2>
+        <p>J'ai ajouté un petit meuble à mon bureau pour ranger que ça tombe pile poil avec le mur.</p>
+        <!--img src="../images/rajout_bureau.png" alt="Rajout Bureau" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>Il est pratique, et tout simple en tasseau. J'avais déjà fait un meuble similiaure pour ranger des casseroles sous l'évier de ma grand-mère.</p>
+    </div>
 
-<!-- meuble chaise tronc -->
-<div class="creation">
-    <h2>Meuble Chaise Tronc</h2>
-    <p>Ce meuble est une chaise originale fabriquée à partir d'un tronc d'arbre, alliant confort et esthétique naturelle.</p>
-    <img src="../images/meuble_chaise_tronc.png" alt="Meuble Chaise Tronc" />
-    <p>Il apporte une touche rustique à mon intérieur.</p>
-</div>
+    <!-- renovation petite table -->
+    <div class="creation">
+        <h2>Rénovation Petite Table Tek </h2>
+        <p>J'ai rénové une petite table en tek, lui rendant toute sa beauté. Je vous laisse juger par vous même.</p>
+        <!--img src="../images/renovation_table_banc_tek.png" alt="Rénovation Petite Table Tek + Banc Extérieur" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>Et un petit banc qui viendra par la suite.</p>
+    </div>
 
-<!-- meuble horloge -->
-<div class="creation">
-    <h2>Meuble Horloge</h2>
-    <p>Ce meuble est une horloge unique, alliant fonctionnalité et design.</p>
-    <img src="../images/meuble_horloge.png" alt="Meuble Horloge
-" />
-    <p>Il ajoute une touche d'élégance à mon intérieur tout en étant pratique.</p>
-</div>
+    <!-- rénovation du banc 1/3 -->
+    <div class="creation">
+        <h2>Rénovation Banc Extérieur 1/3</h2>
+        <p>J'ai rénové un banc d'extérieur en bois, lui redonnant une seconde vie. Première partie je l'ai juste démonté.</p>
+        <!--<img src="../images/renovation_banc_exterieur.png" alt="Rénovation Banc Extérieur" />-->
+        <div class="img-coin bas-droit"></div>
+    </div>
 
-<!-- meuble lampe -->
-<div class="creation">
-    <h2>Meuble Lampe</h2>
-    <p>Ce meuble est une lampe originale, alliant design moderne et fonctionnalité.</p>
-    <img src="../images/meuble_lampe.png" alt="Meuble Lampe" />
-    <p>Il apporte une ambiance chaleureuse et lumineuse à mon espace de vie.</p>
-</div>
+    <!-- planche cuisine -->
+    <div class="creation">
+        <h2>Planche Cuisine</h2>
+        <p>J'ai mis une planche en bois dans la cuisine pour cacher le dessous, adapté sur mesure pour l'espace et surtout pour pouvoir ouvrir le lave-vaisselle.</p>
+        <!--img src="../images/planche_cuisine.png" alt="Planche Cuisine" /-->
+        <div class="img-coin bas-droit"></div>
+        <p>Elle est à la fois pratique et esthétique.</p>
+    </div>
+
+    <!-- rénovation du banc 2/3 -->
+    <div class="creation">
+        <h2>Rénovation Banc Extérieur</h2>
+        <p>Seconde partie, en plein ponçage ma ponceuse m'a lachée.</p>
+        <!--<img src="../images/renovation_banc_exterieur.png" alt="Rénovation Banc Extérieur" />-->
+        <div class="img-coin bas-droit"></div>
+    </div>
+
+    <!-- meuble porte épide -->
+    <div class="creation">
+        <h2>Meuble Porte Épices</h2>
+        <p>Ce meuble est conçu pour organiser et exposer mes épices de manière pratique et esthétique.</p>
+        <!--<img src="../images/meuble_porte_epices.png" alt="Meuble Porte Épices" />-->
+        <div class="img-coin bas-droit"></div>
+        <p>Il est surtout très fonctionnel et un gain de place dans la cuisine.</p>
+    </div>
+
+    <!-- meuble porte clé -->
+    <div class="creation">
+        <h2>Meuble Porte Clés</h2>
+        <p>Encore une solution pratique pour répondre à un besoin car nous avons beaucoup de clé.</p>
+        <!--<img src="../images/meuble_porte_cles.png" alt="Meuble Porte Clés" />-->
+        <div class="img-coin bas-droit"></div>
+        <p>Par contre je l'ai vraiment fait n'importe comment à la va vite.</p>
+    </div>
+
+    <!-- rénovation du banc 3/3 -->
+    <div class="creation">
+        <h2>Rénovation Banc Extérieur</h2>
+        <p>Dernière partie du banc. Couche de lazure et de vernis.</p>
+        <!--<img src="../images/renovation_banc_exterieur.png" alt="Rénovation Banc Extérieur" />-->
+        <div class="img-coin bas-droit"></div>
+        <p>Il s'allie parfaitement à la table.</p>
+    </div>
+
+    <!-- meuble vide poche -->
+    <div class="creation">
+        <h2>Meuble Vide Poche</h2>
+        <p>Ce meuble est conçu pour accueillir mes répondre à plusieurs besoins de rangement sur mon bureau (pot à stylo, téléphone, casque et broutilles).</p>
+        <!--<img src="../images/meuble_vide_poche.png" alt="Meuble Vide Poche" />-->
+        <div class="img-coin bas-droit"></div>
+        <p>Premier meuble pour lequel j'ai fait des plans 3D.</p>
+    </div>
+
+    <!-- meuble table banc, 2 en 1 -->
+    <div class="creation">
+        <h2>Meuble Table-Bancs, 2 en 1</h2>
+        <p>Ce meuble est une pièce polyvalente qui peut servir de table ou de banc selon les besoins, et qui se transforme facilement.</p>
+        <!--<img src="../images/meuble_table_banc.png" alt="Meuble Table Banc" />-->
+        <div class="img-coin bas-droit"></div>
+        <p>Il est idéal pour l'extérieur, il est en douglas et traité pour résister aux intempéries.</p>
+    </div>
+</section>
+
+<section class="projets">
+    <p> Et voici mes futurs projets :</p>
+    <!-- meuble cascade lumineuse -->
+    <div class="creation">
+        <h2>Meuble Cascade Lumineuse</h2>
+        <p>Ce meuble est conçu pour accueillir une cascade lumineuse, uniquement pour l'esthétique.</p>
+        <!--<img src="../images/meuble_cascade_lumineuse.png" alt="Meuble Cascade Lumineuse" />-->
+        <div class="img-coin bas-droit"></div>
+        <p>Il apportera une ambiance chaleureuse et apaisante à mon intérieur.</p>
+    </div>
+
+    <!-- meuble range mug -->
+    <div class="creation">
+        <h2>Meuble Range Mug</h2>
+        <p>Ce meuble est conçu pour organiser et exposer les mugs de manière pratique et esthétique.</p>
+        <!--<img src="../images/meuble_range_mug.png" alt="Meuble Range Mug" />-->
+        <div class="img-coin bas-droit"></div>
+        <p>A la demande de ma copine, j'ai pensé tout le reste.</p>
+    </div>
+
+    <!-- meuble chaise tronc -->
+    <div class="creation">
+        <h2>Meuble Chaise Tronc</h2>
+        <p>Ce meuble est une chaise originale fabriquée à partir d'un tronc d'arbre.</p>
+        <!--<img src="../images/meuble_chaise_tronc.png" alt="Meuble Chaise Tronc" />-->
+        <div class="img-coin bas-droit"></div>
+        <p>Très simple, il me faut juste trouver le trons adéquat.</p>
+    </div>
+
+    <!-- meuble horloge -->
+    <div class="creation">
+        <h2>Meuble Horloge</h2>
+        <p>Ce meuble est une horloge unique, purement esthétique et design.</p>
+        <!--<img src="../images/meuble_horloge.png" alt="Meuble Horloge" />-->
+        <div class="img-coin bas-droit"></div>
+        <p>Il me faut juste trouver le mécanisme d'horloge.</p>
+    </div>
+
+    <!-- meuble lampe -->
+    <div class="creation">
+        <h2>Meuble Lampe</h2>
+        <p>Ce meuble est une lampe originale.</p>
+        <!--<img src="../images/meuble_lampe.png" alt="Meuble Lampe" />-->
+        <div class="img-coin bas-droit"></div>
+        <p>Il me faut juste trouver le fil et une douille pour l'ampoule.</p>
+    </div>
+
+    <!-- compost -->
+    <div class="creation">
+        <h2>Compost</h2>
+        <p>Il s'agit d'un composteur.</p>
+        <!--<img src="../images/meuble_compost.png" alt="Meuble Compost"/>-->
+        <div class="img-coin bas-droit"></div>
+        <p>A la demande de mes parents, car celui donné par la mairie est trop grand.</p>
+</section>
 
 <?php include("./footer.php"); ?>
