a49d7f8187981e53b95be8668ecc451455de15c4
Bastien-Project <b.thomas30712@gmail.com>
2025-07-26 14:25:47 +0200

    FIX : css de la page des autres passions

--- pages/sasp.php
+++ pages/sasp.php
@@ -2,25 +2,26 @@
 $base = '../';
 include("./header.php"); ?>
 
-<p>Voici quelques-unes de mes passions :</p>
-<div class="sportAutomobile">
-    <h2>Sport Automobile</h2>
-    <p>J'adore le sport automobile, que ce soit la F1 ou le karting que je pratique en loisir.</p>
-    <img src="../images/karting.png" alt="Sport Automobile" />
-    <p>Vous pouvez en apprendre plus sur mes expériences et mes projets dans ce domaine.</p>
-</div>
-<div class="ski">
-    <h2>Ski</h2>
-    <p>Le ski est une de mes passions depuis mon enfance, et chaque hiver j'attends avec impatience de pouvoir skier.</p>
-    <img src="../images/ski.png" alt="Ski" />
-    <p>Découvrez mes aventures sur les pistes et mes conseils pour les passionnés de ski
-    </p>
-</div>
-<div class="code">
-    <h2>Code et Programmation</h2>
-    <p>Je suis passionné par le code et la programmation, et j'aime partager mes projets de développement web.</p>
-    <img src="../images/webastien.png" alt="Code et Programmation" />
-    <p>Explorez mes projets, tutoriels et réflexions sur le développement web.</p>
-</div>
+<section class="autresPassions">
+    <p>Voici brièvement mes autres passions :</p>
+    <div class="sportAutomobile">
+        <h2>Sport Automobile</h2>
+        <p>J'adore le sport automobile, que ce soit la F1 ou le karting que je pratique en loisir.</p>
+        <img src="../images/karting.png" alt="Sport Automobile" />
+        <p>Me voici avec des collègues, juste avant le départ d'une endurance de 6 heures dont nous finirons deuxième (à 8 secondes devant les troisièmes).</p>
+    </div>
+    <div class="ski">
+        <h2>Ski</h2>
+        <p>Le ski est une de mes passions depuis mon enfance, et chaque hiver j'attends avec impatience de pouvoir skier.</p>
+        <img src="../images/ski.png" alt="Ski" />
+        <p>C'est moi à <a href="https://live.lumiplan.pro/" target="_blank">Méribel</a>, sur la piste du raffort. La photo n'est pas en noir et blanc, c'est juste le ciel gris et la neige qui tombait qui font cet effet.</p>
+    </div>
+    <div class="code">
+        <h2>Code et Programmation</h2>
+        <p>Je suis passionné par le code et la programmation, et j'aime partager mes projets de développement web.</p>
+        <img src="../images/webastien.png" alt="Code et Programmation" />
+        <p>Vous pouvez retrouver tout mes sites et projets sur mon <a href="https://github.com/Bastien-Project" target="_blank">Github</a> ou sur <a href="https://webastien.42web.io/" target="_blank">mon site personnel</a>.</p>
+    </div>
+</section>
 
 <?php include("./footer.php"); ?>
